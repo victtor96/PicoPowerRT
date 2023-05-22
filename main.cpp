3fcb8ce63ebfd95ded7773903823987725e76295
Lucas Hartmann <lucas.hartmann@cear.ufpb.br>
2023-05-22 18:24:52 -0300

    Minor hooks cleanup.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,6 @@
 #include <stdio.h>
 #include <pico/stdio.h>
 
-void vApplicationMallocFailedHook( void );
-void vApplicationIdleHook( void );
-void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
-void vApplicationTickHook( void );
-
 int main( void ) {
     stdio_init_all();
     printf("FreeRTOS-SMP started.\n");
